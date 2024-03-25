af38fe0547ca0ecf8cdeaac09cb26a8c073ebd66
Andrey Barsky <andreybarsky@gmail.com>
2024-03-25 13:55:03 +0100

    cleanup

--- config.py
+++ config.py
@@ -5,21 +5,18 @@ json_dir =  '/home/abarsky/data/annotation_sets'
 image_dir = '/home/abarsky/data/annotation_sets/imgs'
 label_dir = '/home/abarsky/data/annotation_sets/boxes'
 
-# set the numeric units in which annotations are saved:
-fractional_bounding_boxes = True
-#   if True, annotation bounds are saved as relative fractions of image size
-#     (i.e. between 0 and 1)
-#   if False, they are saved in pixel units instead.
+# maximum size of displayed images on screen: reduce this if the images do not
+# fit on your screen, or increase it if they are too small to read:
+max_display_size= (1500,900)
 
 # set the default colour of generic (classless) bounding boxes, in integer RGB:
 default_colour = (255, 100, 0) # orange ish
 
 
-# maximum size of displayed images on screen: reduce this if the images do not
-# fit on your screen, or increase it if they are too small to read:
-max_display_size= (1500,900)
 
-#### parameters below are only relevant for multi-class annotation
+
+
+#### parameters below are only relevant for multi-class annotation (untested after refactoring)
 
 # this dict maps class label names to the colours of their bounding boxes.
 # to add a new class, you can add a new dict entry:
